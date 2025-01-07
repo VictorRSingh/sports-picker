b1fd8d54a8de3ba2edee97a42ab0c1d890ba1606
Victor <vsingh57@myseneca.ca>
2025-01-06 21:12:03 -0500

    Formatting Table

--- src/utils/NFLSection.tsx
+++ src/utils/NFLSection.tsx
@@ -3,50 +3,61 @@ import PlayerGraphs from "@/components/PlayerGraphs";
 import { GameLog } from "@/interfaces/GameLog";
 
 interface NFLProps {
-    gamelog: GameLog[];
-    position: string;
-    overUnder: any;
-    setOverUnder: React.Dispatch<React.SetStateAction<any>>;
-  }
-  
-  export const NFLSection: React.FC<NFLProps> = ({ gamelog, position, overUnder, setOverUnder }) => {
-    const positionMapping: Record<string, (keyof GameLog)[]> = {
-      QUARTERBACK: ["passingYards", "completions", "rushingYards"],
-      RUNNING_BACK: ["rushingAttempts", "rushingYards", "rushingYardsPerAttemptAverage"],
-      WIDE_RECEIVER: ["receptions", "receivingYards", "receivingTouchdowns"],
-      TIGHT_END: ["receptions", "receivingYards"],
-    };
-  
-    const stats = positionMapping[position] || [];
-  
-    return (
-      <>
-        {stats.map((stat) => (
-          <div className="w-full" key={stat}>
-            <OverUnderInput
-              label={`Over/Under: ${stat}`}
-              value={overUnder[position.toLowerCase()][stat]} // Use proper type or cast
-              onChange={(value) =>
-                setOverUnder((prev: any) => ({
-                  ...prev,
-                  nfl: {
-                    ...prev.nfl,
-                    [position.toLowerCase()]: {
-                      ...prev.nfl[position.toLowerCase()],
-                      [stat]: value,
-                    },
+  gamelog: GameLog[];
+  position: string;
+  overUnder: any;
+  setOverUnder: React.Dispatch<React.SetStateAction<any>>;
+}
+
+export const NFLSection: React.FC<NFLProps> = ({
+  gamelog,
+  position,
+  overUnder,
+  setOverUnder,
+}) => {
+  const positionMapping: Record<string, (keyof GameLog)[]> = {
+    QUARTERBACK: ["passingYards", "completions", "rushingYards"],
+    RUNNING_BACK: [
+      "rushingAttempts",
+      "rushingYards",
+      "rushingYardsPerAttemptAverage",
+    ],
+    WIDE_RECEIVER: ["receptions", "receivingYards", "receivingTouchdowns"],
+    TIGHT_END: ["receptions", "receivingYards"],
+  };
+
+  const stats = positionMapping[position] || [];
+
+  return (
+    <>
+      {stats.map((stat) => (
+        <div className="mt-4" key={stat}>
+          <OverUnderInput
+            label={`Over/Under: ${stat}`}
+            value={overUnder[position.toLowerCase()][stat]} // Use proper type or cast
+            onChange={(value) =>
+              setOverUnder((prev: any) => ({
+                ...prev,
+                nfl: {
+                  ...prev.nfl,
+                  [position.toLowerCase()]: {
+                    ...prev.nfl[position.toLowerCase()],
+                    [stat]: value,
                   },
-                }))
-              }
-            />
+                },
+              }))
+            }
+          />
+
+          <div className="border-2 border-gray-300 h-full">
             <PlayerGraphs
               gamelog={gamelog}
               statistic={stat}
               overUnder={overUnder[position.toLowerCase()][stat]}
             />
           </div>
-        ))}
-      </>
-    );
-  };
-  
+        </div>
+      ))}
+    </>
+  );
+};
