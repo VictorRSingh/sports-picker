071d52957ba7d594643278e2b99deac35bd727fe
Victor <vsingh57@myseneca.ca>
2025-01-08 13:42:18 -0500

    Changed route.ts

--- src/app/api/bettingpros/nba/props/[player]/[props]/route.ts
+++ src/app/api/bettingpros/nba/props/[player]/[props]/route.ts
@@ -1,8 +1,10 @@
 // app/sportsPicker/bettingpros/nba/props/[player]/[prop]/route.ts
 import { NextRequest, NextResponse } from "next/server";
 
-export async function GET(request: NextRequest, { params }: { params: { player: string; prop: string } }) {
-  const { player, prop } = params;
+export async function GET(request: NextRequest ) {
+  const { searchParams } = new URL(request.url);
+  const player = searchParams.get("player");
+  const prop = searchParams.get("prop");
 
   return NextResponse.json({
     message: `Hello ${player}, your prop is ${prop}`,
