faf415c2becc1619fc516177822fcd53a83e05b1
Victor Singh <vsingh57@myseneca.ca>
2025-01-08 18:16:01 +0000

    from pi

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
-  basePath: "/sportsPredictor",
-  assetPrefix: "/sportsPredictor"
+  basePath: "/sportsPicker",
+  assetPrefix: "/sportsPicker"
 };
 
 export default nextConfig;
