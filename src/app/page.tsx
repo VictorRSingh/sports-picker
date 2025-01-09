835dc593a1e997b70257d88f3c7a84834bf61efe
Victor <vsingh57@myseneca.ca>
2025-01-09 02:13:34 -0500

    Added checks for supported sports

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,23 +15,32 @@ export default function Home() {
     <div className="flex flex-col md:flex-row max-h-screen overflow-hidden">
       {/* Sidebar */}
       <div className="flex flex-col items-center gap-y-4 md:w-1/3 border-b-2 md:border-r-2 md:border-b-0 p-4">
-        <PlayerSearch
-        player={player}
-        setPlayer={setPlayer}
-        />
+        <PlayerSearch player={player} setPlayer={setPlayer} />
         {player && <PlayerView playerData={player} />}
 
-        {gameLogs.length > 0 && player && <PlayerGraphStatistics playerObject={player}  gameLogs={gameLogs} />}
+        {gameLogs.length > 0 && player && (
+          <PlayerGraphStatistics playerObject={player} gameLogs={gameLogs} />
+        )}
       </div>
 
       {/* Main Content */}
       <div className="flex flex-grow overflow-auto md:w-2/3">
         {player && (
-          <div className="w-full overflow-x-auto text-sm max-h-[100%]">
-            <PlayerGameData player={player} gameLogs={gameLogs} setGameLogs={setGameLogs} />
-          </div>
+          <>
+            {player.sport === "nba" || player.sport === "nfl" ? (
+              <div className="w-full overflow-x-auto text-sm max-h-[100%]">
+                <PlayerGameData
+                  player={player}
+                  gameLogs={gameLogs}
+                  setGameLogs={setGameLogs}
+                />
+              </div>
+            ) : (
+              <div className="w-full h-full flex justify-center items-center">Sport not supported.</div>
+            )}
+          </>
         )}
       </div>
     </div>
-  ); 
+  );
 }
--- src/components/PlayerGameLogs.tsx
+++ src/components/PlayerGameLogs.tsx
@@ -1,57 +1,55 @@
-import { GameLog } from '@/interfaces/GameLog';
-import { Player } from '@/interfaces/Player';
-import { getHeaders } from '@/utils/getHeaders';
-import { getRowData } from '@/utils/getRowData';
-import React from 'react'
+import { GameLog } from "@/interfaces/GameLog";
+import { Player } from "@/interfaces/Player";
+import { getHeaders } from "@/utils/getHeaders";
+import { getRowData } from "@/utils/getRowData";
+import React from "react";
 
 interface PlayerGameLogsProps {
-    player: Player | null;
-    gameLogs: GameLog[];
+  player: Player | null;
+  gameLogs: GameLog[];
 }
-const PlayerGameLogs = ({ player, gameLogs}: PlayerGameLogsProps) => {
-    
+const PlayerGameLogs = ({ player, gameLogs }: PlayerGameLogsProps) => {
   const headers = getHeaders(player);
   return (
     <div className="overflow-x-auto max-w-full">
-    <h1 className="mb-4"></h1>
-    <div className="max-h-40">
-      <table className="w-full border-collapse border ">
-        <thead>
-          <tr className="grid grid-cols-[auto,auto,repeat(15,auto)] gap-2">
-            <th className="w-16">GAME</th>
-            <th className="w-16">W/L</th>
-            {headers.map((header, index) => (
-              <th key={index} className="w-16">
-                {header}
-              </th>
-            ))}
-          </tr>
-        </thead>
-        <tbody>
-          {gameLogs.map((game, index) => {
-            const rowData = getRowData(game, player);
-            return (
-              <tr
-                key={index}
-                className="grid grid-cols-[auto,auto,repeat(15,auto)] gap-2 border-t"
-              >
-                <td className="w-16 flex justify-center">
-                  {game.opposition}
-                </td>
-                <td className="w-16 flex justify-center">{game.winLose}</td>
-                {rowData!.map((data, idx) => (
-                  <td key={idx} className="w-16 flex justify-center">
-                    {data}
+      <div className="max-h-40">
+        <table className="w-full border-collapse border ">
+          <thead>
+            <tr className="grid grid-cols-[auto,auto,repeat(15,auto)] gap-2">
+              <th className="w-16">GAME</th>
+              <th className="w-16">W/L</th>
+              {headers.map((header, index) => (
+                <th key={index} className="w-16">
+                  {header}
+                </th>
+              ))}
+            </tr>
+          </thead>
+          <tbody>
+            {gameLogs.map((game, index) => {
+              const rowData = getRowData(game, player);
+              return (
+                <tr
+                  key={index}
+                  className="grid grid-cols-[auto,auto,repeat(15,auto)] gap-2 border-t"
+                >
+                  <td className="w-16 flex justify-center">
+                    {game.opposition}
                   </td>
-                ))}
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
+                  <td className="w-16 flex justify-center">{game.winLose}</td>
+                  {rowData!.map((data, idx) => (
+                    <td key={idx} className="w-16 flex justify-center">
+                      {data}
+                    </td>
+                  ))}
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
     </div>
-  </div>
-  )
-}
+  );
+};
 
-export default PlayerGameLogs
+export default PlayerGameLogs;
